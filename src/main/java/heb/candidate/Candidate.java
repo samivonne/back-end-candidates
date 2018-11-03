de386c64e062d257c6f953eb5803ab7d68a06463
unknown <tbui1@trinity.edu>
2018-11-02 22:31:31 -0500

    Added new branch for task 47

--- src/main/java/heb/candidate/Candidate.java
+++ src/main/java/heb/candidate/Candidate.java
@@ -2,11 +2,13 @@ package heb.candidate;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Table;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
+@Table(name = "candidate")
 public class Candidate {
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
