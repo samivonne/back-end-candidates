890f1c1e42c28f6fa6a2b0fe5d6696692ea53ce8
unknown <tbui1@trinity.edu>
2018-11-02 20:25:04 -0500

    Add database structure and mock data

---
+++ DatabaseStuffs/candidateDB.sql
@@ -0,0 +1,47 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+-- -----------------------------------------------------
+-- Schema heb
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema heb
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `heb` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
+USE `heb` ;
+
+-- -----------------------------------------------------
+-- Table `heb`.`candidate`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `heb`.`candidate` ;
+
+CREATE TABLE IF NOT EXISTS `heb`.`candidate` (
+  `userId` INT(11) NOT NULL AUTO_INCREMENT,
+  `userName` VARCHAR(20) NOT NULL,
+  `firstName` VARCHAR(20) NULL DEFAULT NULL,
+  `lastName` VARCHAR(20) NULL DEFAULT NULL,
+  `email` VARCHAR(30) NULL DEFAULT NULL,
+  `streetAddress` VARCHAR(20) NULL DEFAULT NULL,
+  `zipCode` VARCHAR(10) NULL DEFAULT NULL,
+  `state` VARCHAR(20) NULL DEFAULT NULL,
+  `city` VARCHAR(20) NULL DEFAULT NULL,
+  `phoneNumber` VARCHAR(12) NULL DEFAULT NULL,
+  `githubLink` VARCHAR(50) NULL DEFAULT NULL,
+  `password` VARCHAR(20) NULL DEFAULT NULL,
+  PRIMARY KEY (`userId`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 0
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
---
+++ DatabaseStuffs/mockData.sql
@@ -0,0 +1,37 @@
+-- MySQL dump 10.13  Distrib 8.0.13, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: heb
+-- ------------------------------------------------------
+-- Server version	8.0.13
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+ SET NAMES utf8 ;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping data for table `candidate`
+--
+
+LOCK TABLES `candidate` WRITE;
+/*!40000 ALTER TABLE `candidate` DISABLE KEYS */;
+INSERT INTO `candidate` VALUES (1,'tbui1','Thoai','Bui','tbui1@trinity.edu','123 Trinity Pl','78238','TX','San Antonio','123-456-7890','abcd','123456'),(2,'christianm','Christian','Morales','christian@trinity.edu','240 Trinity Pl','78238','TX','San Antonio','420-242-4321','tuvw','123213'),(3,'karlmenzel','Karl','Menzel','karl@trinity.edu','789 Trinity Pl','78238','TX','San Antonio','789-123-4560','hijk','456321'),(4,'jerrygonzalez','Jerry','Gonzalez','jerry@trintiy.edu','110 Trinity Pl','78238','TX','San Antonio','321-231-4312','lmno','546231'),(5,'samivonne','Sam','Ortiz','sam@trinity.edu','456 Trinity Pl','78238','TX','San Antonio','456-789-1234','defg','654321'),(6,'brianw','brian','Wong','Brian@trinity.edu','320 Trinity Pl','78238','TX','San Antonio','214-123-3221','pqrs','213412');
+/*!40000 ALTER TABLE `candidate` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-11-02 20:12:55
